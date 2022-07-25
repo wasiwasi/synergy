c092e7bfe943cf62469a5f5509dc3e399296c9c9
hyeokho <honorable110@gmail.com>
2022-07-25 17:34:24 +0900

    FEAT: Init project

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,18 @@
+import Header from '../components/common/Header';
+import { Link, Outlet } from 'react-router-dom';
+
+const Login = () => {
+// const HomePage: React.FC = () => {
+  return (
+    <div>
+      {/* <Header></Header> */}
+      <h4>Login</h4>
+      <h4>Login</h4>
+      <h4>Login</h4>
+    </div>
+
+  );
+  
+};
+
+export default Login;
