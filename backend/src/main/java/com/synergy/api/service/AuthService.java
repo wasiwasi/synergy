129aa469ce1f45110a844d1bb5dd36662e8c5091
Jeon Jongmin <ho0524870@gmail.com>
2022-07-27 14:09:56 +0900

    FEAT : login basic developed login feature can be tested (req -> res) modified UserRepository, it must be conflicted

--- backend/src/main/java/com/synergy/api/service/AuthService.java
+++ backend/src/main/java/com/synergy/api/service/AuthService.java
@@ -3,6 +3,7 @@ package com.synergy.api.service;
 import com.synergy.api.request.UserLoginPostReq;
 import com.synergy.api.response.TokenRes;
 import com.synergy.common.auth.RefreshToken;
+import com.synergy.common.auth.UserDetails;
 import com.synergy.common.util.JwtTokenUtil;
 import com.synergy.config.TokenConfig;
 import com.synergy.db.entity.User;
@@ -10,12 +11,16 @@ import com.synergy.db.repository.LogoutAccessTokenRepository;
 import com.synergy.db.repository.RefreshTokenRedisRepository;
 import com.synergy.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -28,20 +33,35 @@ public class AuthService {
 
     public TokenRes login(UserLoginPostReq req) {
         // 이메일과 비밀번호 모두 비어서는 안됨
-        if(req.getEmail() == null || req.getEmail().equals("") ||
-                req.getPassword() == null || req.getPassword().equals(""))
-            throw new IllegalArgumentException("이메일과 비밀번호를 입력해주세요");
+//        if(req.getEmail() == null || req.getEmail().equals("") ||
+//                req.getPassword() == null || req.getPassword().equals(""))
+//            throw new IllegalArgumentException("이메일과 비밀번호를 입력해주세요");
 
-        // 사용자 이메일로 (현재 기존 코드는 userId로 찾는다) 사용자가 존재하는지 확인
-        User user = userRepository.findByUserId(req.getEmail()).orElseThrow(NoSuchElementException::new);
+        if(req.getEmail() == null || req.getPassword() == null)
+            throw new IllegalArgumentException("please enter email and password");
 
-        // 비밀번호가 맞는지 확인
-        checkPassword(req.getPassword(), user.getPassword());
+        log.info("req email: "+req.getEmail()+" req password: "+req.getPassword());
+
+        // 사용자 이메일로 사용자가 존재하는지 확인
+        User user = userRepository.findByEmail(req.getEmail()).orElseThrow(NoSuchElementException::new);
+
+        log.info("user info:"+user.toString());
+
+        // 입력으로 들어온 비밀번호와 DB에 저장된 암호를 비교해 비밀번호가 맞는지 확인
+        //checkPassword(req.getPassword(), user.getPassword());
+
+        //
+        String accessToken = JwtTokenUtil.getToken(req.getEmail());
+        log.debug("created access token "+accessToken);
+        RefreshToken refreshToken = saveRefreshToken(req.getEmail());
+        log.debug("created refresh token "+refreshToken);
+
+        return new TokenRes(accessToken, refreshToken.getRefreshToken());
     }
 
     public void checkPassword(String rawPass, String encodedPassword) {
         if(!passwordEncoder.matches(rawPass, encodedPassword)) {
-            throw new IllegalArgumentException("비밀번호가 맞지 않습니다");
+            throw new IllegalArgumentException("wrong password");
         }
     }
 
@@ -52,11 +72,25 @@ public class AuthService {
 
     // TODO: 여기부터 다시 시작
     public String resolveToken(String token) {
-        return null;
+        int headerLength = 7;
+        return token.substring(headerLength);
     }
 
     public TokenRes reissue(String refreshToken) {
-        return null;
+        refreshToken = resolveToken(refreshToken);
+        String email = getCurrentUserEmail();
+        RefreshToken redisRefreshToken = refreshTokenRedisRepository.findById(email).orElseThrow(NoSuchElementException::new);
+
+        if(refreshToken.equals(redisRefreshToken.getRefreshToken())) {
+            return reissueRefreshToken(refreshToken, email);
+        }
+        throw new IllegalArgumentException("토큰이 일치하지 않습니다");
+    }
+
+    private String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        return principal.getUsername();
     }
 
     public TokenRes reissueRefreshToken(String refreshToken, String userId) {
--- backend/src/main/java/com/synergy/db/repository/RefreshTokenRedisRepository.java
+++ backend/src/main/java/com/synergy/db/repository/RefreshTokenRedisRepository.java
@@ -2,8 +2,8 @@ package com.synergy.db.repository;
 
 import com.synergy.common.auth.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RefreshTokenRedisRepository extends JpaRepository<RefreshToken, String> {
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
 }
