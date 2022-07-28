4ba7dcbc15374c653336226b1f2c91f7d36c1cd3
Hyeokho <honorable110>
2022-07-28 14:04:21 +0900

    TEST: jenkins push test

---
+++ frontend/src/pages/CreateChannel.tsx
@@ -0,0 +1,18 @@
+import Header from '../components/common/Header';
+import { Link, Outlet } from 'react-router-dom';
+
+const CreateChannel = () => {
+// const HomePage: React.FC = () => {
+  return (
+    <div>
+      {/* <Header></Header> */}
+      <h4>CreateChannel</h4>
+      <h4>CreateChannel</h4>
+      <h4>CreateChannel</h4>
+    </div>
+
+  );
+  
+};
+
+export default CreateChannel;
