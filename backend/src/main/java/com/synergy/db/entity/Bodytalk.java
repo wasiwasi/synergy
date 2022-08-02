fff614c02646c3a446577089f75b7d4fd3b3e795
강봉민 <kbm4250@naver.com>
2022-08-03 00:28:30 +0900

    FEAT: Add Subject Entity

---
+++ backend/src/main/java/com/synergy/db/entity/Bodytalk.java
@@ -0,0 +1,23 @@
+package com.synergy.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Bodytalk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bodytalk_id")
+    Long id;
+
+    String word;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_set_id")
+    SubjectSet subjectSet;
+}
