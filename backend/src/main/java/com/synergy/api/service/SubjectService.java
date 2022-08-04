f025fe966ca6782cc882e2cf193f3aac0b6e0bf2
강봉민 <kbm4250@naver.com>
2022-08-04 11:36:25 +0900

    FEAT: Add JWT verify in Subject API

--- backend/src/main/java/com/synergy/api/service/SubjectService.java
+++ backend/src/main/java/com/synergy/api/service/SubjectService.java
@@ -1,12 +1,14 @@
 package com.synergy.api.service;
 
 import com.synergy.db.entity.Bodytalk;
+import com.synergy.db.entity.SubjectSet;
 import com.synergy.db.entity.SubjectSetDto;
 
 import java.util.List;
 
 public interface SubjectService {
-    public List<SubjectSetDto> getSubjectSet(List<Long> ids);
+    public List<SubjectSetDto> getSubjectSets(List<Long> ids);
 
     public List<Bodytalk> getBodytalk(Long subjectId);
+    public SubjectSet getSubjectSet(Long subjectId);
 }
