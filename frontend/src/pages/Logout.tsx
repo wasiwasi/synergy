7a3ecb9be91530440a2f02090f2af72cb41f51d7
강봉민 <kbm4250@naver.com>
2022-08-09 20:01:31 +0900

    FEAT: Impl Logout page

---
+++ frontend/src/pages/Logout.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from 'axios'
+
+const Logout = () => {
+
+    const BE_URL = process.env.REACT_APP_BACKEND_URL;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("Test");
+        axios
+        .post(`${BE_URL}/auth/logout`, JSON.stringify({
+            accessToken : localStorage.getItem("access-token")
+        }))
+            .then(() => {
+            console.log("로그아웃 성공");
+        })
+
+        localStorage.removeItem("access-token");
+        navigate("/");
+    }, []);
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Logout;
