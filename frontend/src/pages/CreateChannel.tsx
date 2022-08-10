fe07bfa937113c8a479bb7f956dea122c46fd70e
강봉민 <kbm4250@naver.com>
2022-08-10 11:03:41 +0900

    FIX: resolve delay rendering when new subscriber join

--- frontend/src/pages/CreateChannel.tsx
+++ frontend/src/pages/CreateChannel.tsx
@@ -180,10 +180,9 @@ function SwipeableTextMobileStepper() {
       // Subscribe to the Stream to receive it. Second parameter is undefined
       // so OpenVidu doesn't create an HTML video by its own
       var subscriber = mySession?.subscribe(event.stream, "undefined");
-      subscribers.push(subscriber as Subscriber);
-
       // Update the state with the new subscribers
-      setSubscribers(subscribers);
+      subscribers.push(subscriber);
+      setSubscribers([...subscribers]);
     });
 
     // On every Stream destroyed...
